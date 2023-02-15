d85621bbcdd01fd0b9c2bcc61bab110dfab629f7
thangdoggy <hhqthang@gmail.com>
2023-02-15 10:59:58 +0700

    properties

--- src/App.tsx
+++ src/App.tsx
@@ -114,7 +114,7 @@ function App() {
           catchAll={<ErrorComponent />}
           resources={[
             {
-              name: 'property',
+              name: 'properties',
               list: AllProperties,
               show: PropertyDetails,
               create: CreateProperty,
@@ -122,18 +122,18 @@ function App() {
               icon: <VillaOutlined />,
             },
             {
-              name: 'agent',
+              name: 'agents',
               list: Agents,
               show: AgentProfile,
               icon: <PeopleAltOutlined />,
             },
             {
-              name: 'review',
+              name: 'reviews',
               list: Home,
               icon: <StarOutlineRounded />,
             },
             {
-              name: 'message',
+              name: 'messages',
               list: Home,
               icon: <ChatBubbleOutline />,
             },
--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import { Button } from '@pankod/refine-mui';
 
-const CustomButton = () => {
-  return <div>CustomButton</div>;
+import { CustomButtonProps } from 'interfaces/common';
+
+const CustomButton = ({
+  type,
+  title,
+  backgroundColor,
+  color,
+  fullWidth,
+  icon,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <Button
+      sx={{
+        flex: fullWidth ? 1 : 'unset',
+        width: fullWidth ? '100%' : 'fit-content',
+        minWidth: 130,
+        gap: '10px',
+        padding: '10px 15px',
+        fontSize: 16,
+        fontWeight: 600,
+        backgroundColor,
+        color,
+        textTransform: 'capitalize',
+        '&:hover': { opacity: 0.9, backgroundColor },
+      }}
+      onClick={handleClick}
+    >
+      {icon}
+      {title}
+    </Button>
+  );
 };
 
 export default CustomButton;
